237c48c6214851162a78f4426847ec9f2fc3c383
rlaqorrb <80166091+rlaqorrb@users.noreply.github.com>
2022-06-10 02:50:29 +0900

    mobile udate(major)

---
+++ JS/navbar.js
@@ -0,0 +1,11 @@
+$('#aboutUs').on('click', function(){
+  $('.About-Us-list').slideToggle();
+})
+
+$('#Mobile-aboutUs').on('click', function(){
+  $('.Mobile-About-Us-list').slideToggle();
+})
+
+$('#navBtn').on('click',function(){
+  $('.sub-menu').toggleClass('slide-left');
+})
